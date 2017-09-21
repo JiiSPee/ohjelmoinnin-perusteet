bbf6667ac8c14b25fc8c3e9a056fd98d8dcaf002
JiiSPee <jere.parikka@gmail.com>
2017-09-21 15:45:46 +0300

    almost done

---
+++ conditional-statements/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa positiivinen, negatiivinen tai nolla.");
+            Console.WriteLine("syötä luku: ");
+            String userinput;
+            userinput = Console.ReadLine();
+
+            int evaluatedNumber;
+            int.TryParse(userinput, out evaluatedNumber);
+            if(evaluatedNumber < 0)
+            {
+                Console.WriteLine($"Numero {evaluatedNumber} on negatiivinen")
+            }
+            else if(evaluatedNumber > 0)
+            {
+                Console.WriteLine($"Numero {evaluatedNumber} on positiivinen")
+            }
+            else
+            {
+                Console.WriteLine($"Numero {evaluatedNumber} on nolla")
+            }
+
+
+            Console.WriteLine($"Syötit arvon {userinput}");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
