461cbaa02273cb176482afba7cc7a777b16ab77e
JiiSPee <jere.parikka@gmail.com>
2017-10-01 17:30:23 +0300

    if tehtävä 2

---
+++ conditional-statements/ConsoleApp5/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Minkä ikäinen olet?");
+            string userinput1;
+            userinput1 = Console.ReadLine();
+
+            int ikä;
+            int.TryParse(userinput1, out ikä);
+
+            Console.WriteLine("Oletko Mtk:n jäsen? k/e (k=kyllä, e=ei)");
+            string userinput2;
+            userinput2 = Console.ReadLine();
+
+            char mtk;
+            char.TryParse(userinput2, out mtk);
+
+            Console.WriteLine("Oletko opiskelija? k/e");
+            string userinput3;
+            userinput3 = Console.ReadLine();
+
+            char opiskelija;
+            char.TryParse(userinput3, out opiskelija);
+
+            Console.WriteLine("Oletko varusmies? k/e");
+            string userinput4;
+            userinput4 = Console.ReadLine();
+
+            char varusmies;
+            char.TryParse(userinput4, out varusmies);
+
+            if (ikä < 7)
+                Console.WriteLine("0e");
+            else if (mtk == 'k' && opiskelija == 'k')
+                Console.WriteLine("Alennus-%  -60%  Hinta  6,40e");
+            else if (ikä > 7 && ikä < 15)
+                Console.WriteLine("Alennus-%  -50%  Hinta  8.00e");
+            else if (ikä > 65)
+                Console.WriteLine("Alennus-%  -50%  Hinta  8.00e");
+            else if (varusmies == 'k')
+                Console.WriteLine("Alennus-%  -50%  Hinta  8.00e");
+            else if (opiskelija == 'k')
+                Console.WriteLine("Alennus-%  -45%  Hinta  8.80e");
+            else if (mtk == 'k')
+                Console.WriteLine("Alennus-%  -15%  Hinta  13,60e");
+            else
+                Console.WriteLine("16e");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ conditional-statements/Tehtävä4/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laittaa numerot nousevaan järjestykseen");
+            Console.WriteLine("syötä ensimmäinen luku ");
+            String userinput1;
+            userinput1 = Console.ReadLine();
+
+            int a;
+            int.TryParse(userinput1, out a);
+
+            Console.WriteLine("syötä toinen luku ");
+            String userinput2;
+            userinput2 = Console.ReadLine();
+
+            int b;
+            int.TryParse(userinput2, out b);
+
+            Console.WriteLine("syötä kolmas luku ");
+            String userinput3;
+            userinput3 = Console.ReadLine();
+
+            int c;
+            int.TryParse(userinput3, out c);
+
+            if (a < b && b < c)
+            {
+                Console.WriteLine($"{a}, {b}, {c}");
+            }
+            else if (a > b && b > c)
+            {
+                Console.WriteLine($"{c}, {b}, {a}");
+            }
+            else if (a < c && c < b)
+            {
+                Console.WriteLine($"{a}, {c}, {b}");
+            }
+            else if (b < a && a < c)
+            {
+                Console.WriteLine($"{b}, {a}, {c}");
+            }
+            else if (b < c && c < a)
+            {
+                Console.WriteLine($"{b}, {c}, {a}");
+            }
+            else if (c < a && a < b)
+            {
+                Console.WriteLine($"{c}, {a}, {b}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
